20c5a240352ffb6735f5c93b19070766950e658d
Taozheng Yang <johnyang66ytz@gmail.com>
2019-11-12 09:50:49 +0800

    added odom publisher into the program, and tested successfully

--- ompl_test/src/visualization/joint_state_visualizer.cpp
+++ ompl_test/src/visualization/joint_state_visualizer.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include <sensor_msgs/JointState.h>
+#include <tf/transform_broadcaster.h>
 #include <string>
 #include <vector>
 
@@ -7,10 +8,18 @@ int main(int argc, char **argv){
     ros::init(argc, argv, "joint_state_visualizer_node");
     ros::NodeHandle n;
     ros::Publisher joints_pub = n.advertise<sensor_msgs::JointState>("joint_states", 10);
-    
-    
-    ros::Rate r(100);
-    
+    tf::TransformBroadcaster broadcaster;
+    ros::Rate r(50);
+    std::vector<std::string> joints = {"ur10_shoulder_pan_joint", "ur10_shoulder_lift_joint", "ur10_elbow_joint", "ur10_wrist_1_joint", "ur10_wrist_2_joint", "ur10_wrist_3_joint", "left_wheel_joint", "right_wheel_joint", "fl_caster_rotation_joint", "fl_caster_wheel_joint", "fr_caster_rotation_joint", "fr_caster_wheel_joint", "bl_caster_rotation_joint", "bl_caster_wheel_joint", "br_caster_rotation_joint", "br_caster_wheel_joint"};
+    std::vector<double> joint_angles = {0.5, 0.5, 0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    double angle = 0;
+
+    // odom message declarations
+    geometry_msgs::TransformStamped odom_trans;
+    sensor_msgs::JointState joint_state;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_footprint";
+ 
     // keep publishing the message
     while(ros::ok()){ 
         // initialize the joints to be published
@@ -18,8 +27,7 @@ int main(int argc, char **argv){
         mm_joints.header.seq = 0;
         mm_joints.header.stamp = ros::Time::now();
         mm_joints.header.frame_id = "";
-        std::vector<std::string> joints = {"ur10_shoulder_pan_joint", "ur10_shoulder_lift_joint", "ur10_elbow_joint", "ur10_wrist_1_joint", "ur10_wrist_2_joint", "ur10_wrist_3_joint", "left_wheel_joint", "right_wheel_joint", "fl_caster_rotation_joint", "fl_caster_wheel_joint", "fr_caster_rotation_joint", "fr_caster_wheel_joint", "bl_caster_rotation_joint", "bl_caster_wheel_joint", "br_caster_rotation_joint", "br_caster_wheel_joint"};
-        std::vector<double> joint_angles = {0.5, 0.5, 0.5, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+        
         std::cout<<"size:"<<joints.size()<<" size:"<<joint_angles.size();
         for(auto joint=joints.begin(); joint!=joints.end(); joint++){
             mm_joints.name.push_back(*joint);   
@@ -31,8 +39,19 @@ int main(int argc, char **argv){
         }
         mm_joints.velocity = {};
         mm_joints.effort = {};
+
+        // update transform
+        // (moving in a circle with radius=2)
+        odom_trans.header.stamp = ros::Time::now();
+        odom_trans.transform.translation.x = cos(angle)*2;
+        odom_trans.transform.translation.y = sin(angle)*2;
+        odom_trans.transform.translation.z = .7;
+        odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(angle+M_PI/2);
         
+        //send the joint state and transform
+        broadcaster.sendTransform(odom_trans);
         joints_pub.publish(mm_joints);
+        angle += 0.1;
         ros::spinOnce();
         r.sleep();
     }
